7b33983cefe9d5498751043a89df0cd5f9058012
Valeria <vale_0722@live.com>
2024-05-31 00:15:40 -0500

    feat: add Category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        Category::factory()->count(10)->create();
+    }
+}
